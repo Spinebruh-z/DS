a7dadf309caef2878cc8c9e8b7761e0a41df24c5
Spinebruh-z <sarthaksatam.dev@gmail.com>
2025-10-16 14:49:38 +0530

    Fix CI: Make requirements.txt optional and add sample test

---
+++ test_sample.py
@@ -0,0 +1,11 @@
+"""Sample test file for CI pipeline"""
+
+
+def test_sample():
+    """A simple passing test"""
+    assert True
+
+
+def test_addition():
+    """Test basic arithmetic"""
+    assert 1 + 1 == 2
